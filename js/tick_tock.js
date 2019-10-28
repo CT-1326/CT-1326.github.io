589735e055d88ade3de96abf77fcabdecd5c2535
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-10-28 15:36:04 +0900

    Update tick_tock.js

--- js/tick_tock.js
+++ js/tick_tock.js
@@ -2,7 +2,7 @@
 var Check = 0
 var Word = ['개강', '변경', '특별한 날']
 var Dt = {
-  id: [new Date("Aug 29, 2019 00:00:00").getTime(), new Date("Sep 04, 2019 23:59:59").getTime(), new Date("Oct 29, 2019 00:00:00").getTime()]
+  id: [new Date("Aug 29, 2019 00:00:00").getTime(), new Date("Sep 04, 2019 23:59:59").getTime(), new Date("Oct 28, 2019 00:00:00").getTime()]
 };
 
 // Update the count down every 1 second
