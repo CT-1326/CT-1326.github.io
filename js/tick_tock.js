0ed61b099567da72c56490a16594f5f3c0b0025f
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-02-13 16:57:44 +0900

    Refrom page

--- js/tick_tock.js
+++ js/tick_tock.js
@@ -2,28 +2,31 @@
 var Check = 0
 var Word = "데드라인"
 var Dt = {
-  id: new Date("Jun 01, 2020 00:00:00").getTime()
+    id: new Date("Jun 01, 2020 00:00:00").getTime()
 };
 
 // Update the count down every 1 second
 var x = setInterval(function () {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+    // Get today's date and time
+    var now = new Date().getTime();
 
-  var distance = Dt.id - now;
+    var distance = Dt.id - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("jsp").innerHTML = Word + " 까지 " + days + "일 " + hours + "시 " +
-    minutes + "분 " + seconds + "초 남았습니다 "
+    // Display the result in the element with id="demo"
+    document
+        .getElementById("jsp")
+        .innerHTML = Word + " 까지 " + days + "일 " + hours + "시 " + minutes + "분 " +
+                seconds + "초 남았습니다 "
 
-  // If the count down is finished, write some text
-  if (distance < 0 && Check < Word.length)
-    Check += 1;
-}, 1000);
+    // If the count down is finished, write some text
+    if (distance < 0 && Check < Word.length) 
+        Check += 1;
+    }
+, 1000);
